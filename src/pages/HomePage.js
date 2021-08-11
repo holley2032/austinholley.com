72d6a85b3e783ba108e61ec7335ac5ba6fbaa23b
Kevin Holley <holley2032@gmail.com>
2021-08-10 22:39:21 -0400

    Adding a component, some css.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import HomePhoto from "../components/HomePhoto";
 import HomeMainText from "../components/HomeMainText";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Header />
       <HomePhoto />
       <HomeMainText />
+      <Footer />
     </div>
   );
 };
